ff485879826ef77a1ae2f877266605e540509443
atilaykosker <atovarkosker@gmail.com>
2024-07-26 13:08:33 +0300

    chore: Update package.json version to 0.0.4 and add repository information

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Form from './components/Form.vue';
 </script>
 
 <template>
-  <Form msg="Vite + Vue" />
+  <Form form-id="57a1c67b-872d-4183-84e1-b36a696c58f0" :on-loaded="(e) => console.log('loaded :>> ', e)" />
 </template>
 
 <style scoped></style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,14 +1,54 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 
-defineProps<{ msg: string }>();
+onMounted(() => {
+  console.log(`the component is now mounted.`);
+  const formScript = document.createElement('script');
+  formScript.src = 'https://dev.signupnotes-sdk.pages.dev/form.js';
+  document.head.appendChild(formScript);
+});
+
+const props = defineProps<{
+  formId: string;
+  returnUrl?: string;
+  onCompleted?: (data: Record<string, any>) => void;
+  onStepChange?: (step: number) => void;
+  onLoading?: (loading: boolean) => void;
+  onLoaded?: (loaded: boolean) => void;
+}>();
+
+const handleLoaded = (event: CustomEvent<boolean>) => {
+  if (event && props.onLoaded) {
+    props.onLoaded(event.detail);
+  }
+};
+const handleLoading = (event: CustomEvent<boolean>) => {
+  if (event && props.onLoading) {
+    props.onLoading(event.detail);
+  }
+};
+const handleStepChange = (event: CustomEvent<number>) => {
+  if (event && props.onStepChange) {
+    props.onStepChange(event.detail);
+  }
+};
+const handleCompleted = (event: CustomEvent<Record<string, any>>) => {
+  if (event && props.onCompleted) {
+    props.onCompleted(event.detail);
+  }
+};
 </script>
 
 <template>
-  <div>asd</div>
+  <form-element
+    style="width: 100%; min-height: 100%"
+    :form-id="formId"
+    :return-url="props.returnUrl"
+    @submit-data="handleCompleted"
+    @step-change="handleStepChange"
+    @loading="handleLoading"
+    @loaded="handleLoaded"
+  />
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
